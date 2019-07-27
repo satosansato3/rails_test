1b3369b010fa91d05ffaee65ad56a61ea51d631c
koji <koji.sato1242@gmail.com>
2019-07-27 13:54:08 +0900

    introduce devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
         return base_name if @title.blank?
         "#{base_name} | #{@title}"
     end
+
+    def flash_message(messagen, klass)
+        content_tag(:div, class: "alert alert-#{klass}") do
+            concat content_tag(:button, "x", class: "close", data: { dismiss: "alert" })
+            concat raw(message)
+        end
+    end
 end
