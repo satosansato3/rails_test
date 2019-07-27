cbdb4a263bac94b7af3de01941a70164ba3ff701
koji <koji.sato1242@gmail.com>
2019-07-27 15:43:33 +0900

    setup of ActionMailer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
         "#{base_name} | #{@title}"
     end
 
-    def flash_message(messagen, klass)
+    def flash_message(message, klass)
         content_tag(:div, class: "alert alert-#{klass}") do
             concat content_tag(:button, "x", class: "close", data: { dismiss: "alert" })
             concat raw(message)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,13 @@ Rails.application.configure do
     host: "localhost",
     port: 3000
   }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.credentials.SMTP_EMAIL,
+    password: Rails.application.credentials.SMTP_PASSWORD
+  }
 end
