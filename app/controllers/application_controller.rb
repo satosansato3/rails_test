1951eababa426c17b7f240ea69ce07b9973a4473
koji <koji.sato1242@gmail.com>
2019-08-13 00:10:55 +0900

    activate admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    rescue_from SecurityError do |exception|
+        redirect_to root_url, notice: "アクセス権がありません"
+    end
+
     protected
+
+    def authenticate_admin_user!
+        raise SecurityError unless current_user.try(:admin?)
+    end
 
     def configure_permitted_parameters
         added_attr = [:name, :email, :password, :password_confirmation, :remenber_me, :avatar]
