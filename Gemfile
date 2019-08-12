41924da38d088c0610f495c11a40172ecf1e2c8d
koji <koji.sato1242@gmail.com>
2019-08-12 22:51:48 +0900

    add active-admin

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'paperclip', '~> 5.0.0'
 gem 'rails-i18n'
 gem 'kaminari'
 gem 'counter_culture'
+gem 'activeadmin'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require "sidekiq/web"
 
 
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
