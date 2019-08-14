4b2ecb84e7db5390db8aad2f073440ba77343edd
koji <koji.sato1242@gmail.com>
2019-08-14 22:38:53 +0900

    set up for DB

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :posts, only: [:create]
-  mount Sidekiq::Web, at: "/sidekiq"
+  authenticate :user, -> (u) { u.admin? } do
+    mount Sidekiq::Web, at: "/sidekiq"
+  end
 end
